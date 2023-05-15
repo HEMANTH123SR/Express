975e4695a46a7a0768515a3ff608c9065627e695
hemanth <applicationcomputer257@gmail.com>
2023-05-15 13:12:52 +0530

    env

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app=require('./app')
-
+const dotenv = require('dotenv');
+const app = require('./app');
+dotenv.config({path:'./config.env'})
+console.log(process.env);
 const port = 3000;
 
 app.listen(port, () => {
