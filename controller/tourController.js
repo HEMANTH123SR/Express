2b340a789e29c2eb3be9627f3b792f226032ecab
hemanth <applicationcomputer257@gmail.com>
2023-05-16 08:40:44 +0530

    schema

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,90 +1,84 @@
-const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
-);
+const Tour=require('./../models/toursModel')
 
-// exports.checkID=(req,res,next,val)=>{
-//  const id = req.params.id * 1;
-//  const tour = tours.find((el) => el.id === id);
-//  if (!tour) {
-//    return res.send('error');
-//  } 
- 
-// }
+
+
 
 exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tours,
+  //   },
+  // });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-   const tour = tours.find((el) => el.id === id);
-   if (!tour) {
-      res.send('error');
-   }
-   else{
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-   }
+  // const id = req.params.id * 1;
+  //  const tour = tours.find((el) => el.id === id);
+  //  if (!tour) {
+  //     res.send('error');
+  //  }
+  //  else{
+  // res.status(200).json({
+  //   status: 'success',
+  //   // data: {
+    //   tour,
+    // },
+  // });
+  //  }
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours.length + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'Data Created',
-        data: {
-          tours,
-        },
-      });
+exports.createTour =async (req, res) => {
+  try{
+const newTour=await Tour.create(req.body);
+
+ res.status(201).json(
+  {
+    status:'success',
+    data:{
+      Tour:newTour
     }
-  );
+  }
+ )
+}
+catch(err){
+  res.status(400).json({
+    status:"fail",
+    message:err
+  })
+}
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-   const tour = tours.find((el) => el.id === id);
-   if (!tour) {
-      res.send('error');
-   }
-   else{
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: 'updated tour here </>',
-      },
-    })
-  }
+  // const id = req.params.id * 1;
+  //  const tour = tours.find((el) => el.id === id);
+  //  if (!tour) {
+  //     res.send('error');
+  //  }
+  //  else{
+  //   res.status(200).json({
+  //     status: 'success',
+  //     data: {
+  //       tour: 'updated tour here </>',
+  //     },
+  //   })
+  // }
   
 };
 
 exports.deleteTour = (req, res) => {
-   const id = req.params.id * 1;
-   const tour = tours.find((el) => el.id === id);
-   if (!tour) {
-      res.send('error');
-   }
-   else{
-    res.status(202).json({
-      status: 'success',
-      message: 'Tour deleted',
-    })
-  }
+  //  const id = req.params.id * 1;
+  //  const tour = tours.find((el) => el.id === id);
+  //  if (!tour) {
+  //     res.send('error');
+  //  }
+  //  else{
+  //   res.status(202).json({
+  //     status: 'success',
+  //     message: 'Tour deleted',
+  //   })
+  // }
   
 };
 
--- server.js
+++ server.js
@@ -17,42 +17,6 @@ mongoose.connect(DB,{
 })
 .catch(err => console.log(err))
 
-const toursSchema=new mongoose.Schema({
-  name:{
-    type:String,
-    required:[true,'A tour must have name'],
-    unique:true
-  },
-  rating:{
-    type:Number,
-    default:4.5,
-  },
-  price:{
-    type:Number,
-    required:[true,'A tour must have a price']
-  },
-})
-
-const Tour=mongoose.model('Tour',toursSchema)
-
-const testTour=new Tour({
-  name:'the forest hiker is the new runner and he is fucking awseome',
-  rating:4.7,
-  price:490,
-})
-// const testTour = new Tour({
-//   name: 'unique tour name', // Change the name to a unique value
-//   rating: 4.7,
-//   price: 490,
-// });
-
-
-testTour
-.save()
-.then(doc => console.log(doc))
-.catch(err=>console.log(err))
-
-
 
 
 const port = 3000;
